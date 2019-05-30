76f872ce0058d3eb8c29b219f961b740b9612cee
venomkiki <clintonfungai@gmail.com>
2019-05-30 23:24:00 +0300

    altered the welcome page

--- kikiBlog/resources/views/welcome.blade.php
+++ kikiBlog/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>[ UG : kikiBlog]</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -76,20 +76,14 @@
                     @endauth
                 </div>
             @endif
-
+             <div class="container">
+                 <img src="" height="250" width="300" class="img-responsive"/>
+             </div>
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <h3>Welcome to kikiBlog</h3>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
     </body>
