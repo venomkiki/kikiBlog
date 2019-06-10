1ed2f76e70a82f371c159156e0e2823faf9ff74a
venomkiki <clintonfungai@gmail.com>
2019-06-10 12:22:44 +0300

    added a test

--- tests/Feature/BlogTest.php
+++ tests/Feature/BlogTest.php
@@ -22,29 +22,25 @@ class BlogTest extends TestCase
     }
 
 
-    public function blog_create() //test store
+    public function blog_create()
     {
         $post = Blog::create(['title'=>'fifa' , 'content'=>'is the best player']);
         $this->assertEquals('fifa', $post->title);
-        //this is the page user goes to after logging in.
         $this->get('/Blogs');
-        //this is the information displayed on the user's page.
         $this->assertEquals('is the best player',$post->content);
     }
 
-    public function updating_post()
-    {
-        $post = Posts::create(['id'=>'1','title'=>'fifa','content'=>'messi']);
-        $post->title = "fifa";
-        $post->content = "messi is a goat";
-        //When the user hit's the endpoint to update the task
-        $this->put('/Blogs/'.$post->id);
-        //The task should be updated in the database.
-        $this->assertDatabaseHas('blogs',['id'=> $post->id , 'title' => 'fifa', 'content' => 'messi']);
-        $res = $this->get('/Blogs');
-        $res->assertSee($post->title);
-        $res->assertSee('messi', $post->content);
-    }
+//    public function updating_post()
+//    {
+//        $post = Posts::update(['id'=>'1','title'=>'fifa','content'=>'messi']);
+//        $post->title = "fifa";
+//        $post->content = "messi is a goat";
+//        $this->put('/Blogs/'.$post->id);
+//        $this->assertDatabaseHas('blogs',['id'=> $post->id , 'title' => 'fifa', 'content' => 'messi']);
+//        $res = $this->get('/Blogs');
+//        $res->assertSee($post->title);
+//        $res->assertSee('messi', $post->content);
+//    }
 
 
 
