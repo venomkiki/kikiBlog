3964374b2bdd9aa504c341b5a01069551847766d
venomkiki <clintonfungai@gmail.com>
2019-06-12 10:18:04 +0300

    edited the home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,12 @@
 <div class="col-md-8">
     <div class="section">
         <div class="container">
-            <img src="" alt="WEL" width="500" height="600">
+
             <h1>WElcome to MyBlog!!</h1>
+            <br>
+            <p class="">
+                KikiBlog is a simple application built to allow anyone to create content and share it with rest of the world!!
+            </p>
         </div>
     </div>
 </div>
