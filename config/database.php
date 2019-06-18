d4fd764c126482f63dd60a761ac7e174fc3d7038
venomkiki <clintonfungai@gmail.com>
2019-06-18 10:41:50 +0300

    changed the db config

--- config/database.php
+++ config/database.php
@@ -87,6 +87,19 @@ return [
             'prefix' => '',
         ],
 
+
+        'testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_TEST_HOST', 'localhost'),
+            'database' => env('DB_TEST_DATABASE', 'testing_db'),
+            'username' => env('DB_TEST_USERNAME', 'root'),
+            'password' => env('DB_TEST_PASSWORD', 'secret'),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+        ],
+
     ],
 
     /*
